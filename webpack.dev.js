f992d56ebdf06515fefc64a56c80e0747b60f449
Tommaso Mannori <aivirth.dev@gmail.com>
2020-11-26 04:49:52 +0100

    Added configuration options for devserver

--- webpack.dev.js
+++ webpack.dev.js
@@ -9,6 +9,11 @@ module.exports = merge(common, {
         filename: "[name].bundle.js",
         path: path.resolve(__dirname, "dist"),
     },
+    devServer: {
+        contentBase: path.join(__dirname, "dist"),
+        compress: false,
+        port: 9000,
+    },
 
     plugins: [
         new HtmlWebPackPlugin({
